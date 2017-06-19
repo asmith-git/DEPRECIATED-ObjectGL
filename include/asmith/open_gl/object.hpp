090daec6a799b5094dab0d06998b98cf704af973
Adam Smith <adam.george.smith@btinternet.com>
2017-06-19 10:20:28 +0100

    Simplified object class

---
+++ include/asmith/open_gl/object.hpp
@@ -0,0 +1,51 @@
+//	Copyright 2017 Adam Smith
+//	Licensed under the Apache License, Version 2.0 (the "License");
+//	you may not use this file except in compliance with the License.
+//	You may obtain a copy of the License at
+// 
+//	http://www.apache.org/licenses/LICENSE-2.0
+//
+//	Unless required by applicable law or agreed to in writing, software
+//	distributed under the License is distributed on an "AS IS" BASIS,
+//	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//	See the License for the specific language governing permissions and
+//	limitations under the License.
+
+#ifndef ASMITH_OPENGL_OBJECT_HPP
+#define ASMITH_OPENGL_OBJECT_HPP
+
+namespace asmith { namespace gl {
+	
+	/*!
+		\brief Base class for OpenGL objects
+		\author Adam Smith
+		\date Created : 4th November 2015 Modified 19th Jube 2017
+		\version 2.0
+	*/
+	class object {
+	public:
+		enum id_t : GLuint{
+			INVALID_ID = 0
+		};
+	protected:
+		GLuint mID;
+	private:
+		object(const object&) = delete;
+		object(object&&) = delete;
+		object& operator=(const object&) = delete;
+		object& operator=(object&&) = delete;
+	public:
+		object();
+		virtual ~object();
+
+		virtual void create() = 0;
+		virtual void destroy() = 0;
+		virtual bool is_created() const throw();
+
+		id_t get_id() const throw();
+		operator bool() const throw();
+	};
+
+}}
+
+#endif
---
+++ src/asmith/open_gl/object.cpp
@@ -0,0 +1,38 @@
+//	Copyright 2017 Adam Smith
+//	Licensed under the Apache License, Version 2.0 (the "License");
+//	you may not use this file except in compliance with the License.
+//	You may obtain a copy of the License at
+// 
+//	http://www.apache.org/licenses/LICENSE-2.0
+//
+//	Unless required by applicable law or agreed to in writing, software
+//	distributed under the License is distributed on an "AS IS" BASIS,
+//	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//	See the License for the specific language governing permissions and
+//	limitations under the License.
+
+#include "asmith/open_gl/object.hpp"
+
+namespace asmith { namespace gl {
+	
+	// class object
+	
+	object::object() :
+		mID(INVALID_ID)
+	{}
+	
+	object::~object() {
+		
+	}
+	
+	id_t object::get_id() const throw() {
+		return static_cast<id_t>(mID);
+	}
+	
+	operator object::bool() const throw() {
+		return mID != INVALID_ID;
+	}
+
+}}
+
+#endif

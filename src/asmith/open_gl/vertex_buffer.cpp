33c89335324eee65b4e264377b2b5128583a0279
Adam Smith <adam.george.smith@btinternet.com>
2017-06-20 17:38:33 +0100

    Added version numbers to buffer targets

--- src/asmith/open_gl/vertex_buffer.cpp
+++ src/asmith/open_gl/vertex_buffer.cpp
@@ -28,19 +28,27 @@ namespace asmith { namespace gl {
 	static uint8_t buffer_target_to_index(const GLenum aTarget) throw() {
 		switch (aTarget) {
 		case GL_ARRAY_BUFFER				: return 0;
-		case GL_ATOMIC_COUNTER_BUFFER		: return 1;
-		case GL_COPY_READ_BUFFER			: return 2;
-		case GL_COPY_WRITE_BUFFER			: return 3;
-		case GL_DISPATCH_INDIRECT_BUFFER	: return 4;
-		case GL_DRAW_INDIRECT_BUFFER		: return 5;
-		case GL_ELEMENT_ARRAY_BUFFER		: return 6;
-		case GL_PIXEL_PACK_BUFFER			: return 7;
-		case GL_PIXEL_UNPACK_BUFFER			: return 8;
-		case GL_QUERY_BUFFER				: return 9;
-		case GL_SHADER_STORAGE_BUFFER		: return 10;
-		case GL_TEXTURE_BUFFER				: return 11;
-		case GL_TRANSFORM_FEEDBACK_BUFFER	: return 12;
-		case GL_UNIFORM_BUFFER				: return 13;
+		case GL_DRAW_INDIRECT_BUFFER		: return 1;
+		case GL_ELEMENT_ARRAY_BUFFER		: return 2;
+		case GL_PIXEL_PACK_BUFFER			: return 3;
+		case GL_PIXEL_UNPACK_BUFFER			: return 4;
+		case GL_TEXTURE_BUFFER				: return 5;
+		case GL_TRANSFORM_FEEDBACK_BUFFER	: return 6;
+#if ASMITH_GL_VERSION_GE(3,1)
+		case GL_COPY_READ_BUFFER			: return 7;
+		case GL_COPY_WRITE_BUFFER			: return 8;
+		case GL_UNIFORM_BUFFER				: return 9;
+#endif
+#if ASMITH_GL_VERSION_GE(4,2)
+		case GL_ATOMIC_COUNTER_BUFFER		: return 10;
+#endif
+#if ASMITH_GL_VERSION_GE(4,3)
+		case GL_DISPATCH_INDIRECT_BUFFER	: return 11;
+		case GL_SHADER_STORAGE_BUFFER		: return 12;
+#endif
+#if ASMITH_GL_VERSION_GE(4,4)
+		case GL_QUERY_BUFFER				: return 13;
+#endif
 		default								: return BUFFER_TARGET_COUNT;
 		}
 	}
@@ -48,20 +56,28 @@ namespace asmith { namespace gl {
 	static uint8_t index_to_buffer_targer(const GLenum aTarget) throw() {
 		switch (aTarget) {
 		case 0: return GL_ARRAY_BUFFER;
-		case 1: return GL_ATOMIC_COUNTER_BUFFER;
-		case 2: return GL_COPY_READ_BUFFER;
-		case 3: return GL_COPY_WRITE_BUFFER;
-		case 4: return GL_DISPATCH_INDIRECT_BUFFER;
-		case 5: return GL_DRAW_INDIRECT_BUFFER;
-		case 6: return GL_ELEMENT_ARRAY_BUFFER;
-		case 7: return GL_PIXEL_PACK_BUFFER;
-		case 8: return GL_PIXEL_UNPACK_BUFFER;
-		case 9: return GL_QUERY_BUFFER;
-		case 10: return GL_SHADER_STORAGE_BUFFER;
-		case 11: return GL_TEXTURE_BUFFER;
-		case 12: return GL_TRANSFORM_FEEDBACK_BUFFER;
-		case 13: return GL_UNIFORM_BUFFER;
+		case 1: return GL_DRAW_INDIRECT_BUFFER;
+		case 2: return GL_ELEMENT_ARRAY_BUFFER;
+		case 3: return GL_PIXEL_PACK_BUFFER;
+		case 4: return GL_PIXEL_UNPACK_BUFFER;
+		case 5: return GL_TEXTURE_BUFFER;
+		case 6: return GL_TRANSFORM_FEEDBACK_BUFFER;
+#if ASMITH_GL_VERSION_GE(3,1)
+		case 7: return GL_COPY_READ_BUFFER;
+		case 8: return GL_COPY_WRITE_BUFFER;
+		case 9: return GL_UNIFORM_BUFFER;
+#endif
+#if ASMITH_GL_VERSION_GE(4,2)
+		case 10: return GL_ATOMIC_COUNTER_BUFFER;
+#endif
+#if ASMITH_GL_VERSION_GE(4,3)
+		case 11: return GL_DISPATCH_INDIRECT_BUFFER;
+		case 12: return GL_SHADER_STORAGE_BUFFER;
+		case 13: return GL_QUERY_BUFFER;
+#endif
+#if ASMITH_GL_VERSION_GE(4,4)
 		default: return GL_INVALID_ENUM;
+#endif
 		}
 	}
 
@@ -102,12 +118,18 @@ namespace asmith { namespace gl {
 	void vertex_buffer::set_data(const GLvoid* aData, GLsizeiptr aSize) {
 		if(! is_created()) throw std::runtime_error("asmith::gl::vertex_buffer::set_data : Buffer does not exist");
 		if(mUsage == GL_INVALID_ENUM) throw std::runtime_error("asmith::gl::vertex_buffer::set_data : Usage not set");
+#if ASMITH_GL_VERSION_LE(4,4)
 		if(mTarget == GL_INVALID_ENUM) throw std::runtime_error("asmith::gl::vertex_buffer::set_data : Buffer is not bound");
+#endif
 		mSize = aSize;
 #if ASMITH_GL_VERSION_LE(2, 1)		
 		glBufferDataARB(mTarget, aSize, aData, mUsage);
 #else
+#if ASMITH_GL_VERSION_LE(4,4)
 		glBufferData(mTarget, aSize, aData, mUsage);
+#else
+		glNamedBufferData(mID, aSize, aData, mUsage);
+#endif
 #endif
 	}
 

4fe1af1d24d1abcc9d8cbf4dd2804727bd019895
Adam Smith <adam.george.smith@btinternet.com>
2017-06-20 17:10:10 +0100

    Storage for buffer stack

--- src/asmith/open_gl/vertex_buffer.cpp
+++ src/asmith/open_gl/vertex_buffer.cpp
@@ -11,6 +11,7 @@
 //	See the License for the specific language governing permissions and
 //	limitations under the License.
 
+#include <vector>
 #include <stdexcept>
 #include "asmith/open_gl/vertex_buffer.hpp"
 
@@ -19,8 +20,55 @@
 //! \todo Implement named buffers
 
 namespace asmith { namespace gl {
+
+	enum {
+		BUFFER_TARGET_COUNT = 14
+	};
+
+	static uint8_t buffer_target_to_index(const GLenum aTarget) throw() {
+		switch (aTarget) {
+		case GL_ARRAY_BUFFER				: return 0;
+		case GL_ATOMIC_COUNTER_BUFFER		: return 1;
+		case GL_COPY_READ_BUFFER			: return 2;
+		case GL_COPY_WRITE_BUFFER			: return 3;
+		case GL_DISPATCH_INDIRECT_BUFFER	: return 4;
+		case GL_DRAW_INDIRECT_BUFFER		: return 5;
+		case GL_ELEMENT_ARRAY_BUFFER		: return 6;
+		case GL_PIXEL_PACK_BUFFER			: return 7;
+		case GL_PIXEL_UNPACK_BUFFER			: return 8;
+		case GL_QUERY_BUFFER				: return 9;
+		case GL_SHADER_STORAGE_BUFFER		: return 10;
+		case GL_TEXTURE_BUFFER				: return 11;
+		case GL_TRANSFORM_FEEDBACK_BUFFER	: return 12;
+		case GL_UNIFORM_BUFFER				: return 13;
+		default								: return BUFFER_TARGET_COUNT;
+		}
+	}
+
+	static uint8_t index_to_buffer_targer(const GLenum aTarget) throw() {
+		switch (aTarget) {
+		case 0: return GL_ARRAY_BUFFER;
+		case 1: return GL_ATOMIC_COUNTER_BUFFER;
+		case 2: return GL_COPY_READ_BUFFER;
+		case 3: return GL_COPY_WRITE_BUFFER;
+		case 4: return GL_DISPATCH_INDIRECT_BUFFER;
+		case 5: return GL_DRAW_INDIRECT_BUFFER;
+		case 6: return GL_ELEMENT_ARRAY_BUFFER;
+		case 7: return GL_PIXEL_PACK_BUFFER;
+		case 8: return GL_PIXEL_UNPACK_BUFFER;
+		case 9: return GL_QUERY_BUFFER;
+		case 10: return GL_SHADER_STORAGE_BUFFER;
+		case 11: return GL_TEXTURE_BUFFER;
+		case 12: return GL_TRANSFORM_FEEDBACK_BUFFER;
+		case 13: return GL_UNIFORM_BUFFER;
+		default: return GL_INVALID_ENUM;
+		}
+	}
+
+	static std::vector<std::weak_ptr<vertex_buffer>> VERTEX_BUFFER_STACK [BUFFER_TARGET_COUNT];
 	
 	// vertex_buffer
+
 	vertex_buffer::vertex_buffer() :
 		mTarget(GL_INVALID_ENUM),
 		mSize(0),

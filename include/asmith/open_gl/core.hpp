0d1e6e9b2f99d034ee9c1611cd5e19471c025554
Adam Smith <adam.george.smith@btinternet.com>
2017-06-27 07:59:24 +0100

    OpenGL type helper

--- include/asmith/open_gl/core.hpp
+++ include/asmith/open_gl/core.hpp
@@ -62,4 +62,37 @@ namespace asmith { namespace gl {
 }}
 #endif
 
+namespace asmith { namespace gl {
+	namespace implementation {
+		template<const GLenum T>
+		struct enum_to_type_ {
+			typedef void type;
+		};
+
+		template<> struct enum_to_type_<GL_UNSIGNED_BYTE> { typedef GLubyte type; };
+		template<> struct enum_to_type_<GL_BYTE> { typedef GLbyte type; };
+		template<> struct enum_to_type_<GL_UNSIGNED_SHORT> { typedef GLushort type; };
+		template<> struct enum_to_type_<GL_SHORT> { typedef GLshort type; };
+		template<> struct enum_to_type_<GL_UNSIGNED_INT> { typedef GLuint type; };
+		template<> struct enum_to_type_<GL_INT> { typedef GLint type; };
+		template<> struct enum_to_type_<GL_FLOAT> { typedef GLfloat type; };
+		template<> struct enum_to_type_<GL_DOUBLE> { typedef GLdouble type; };
+	}
+
+	template<const GLenum T>
+	using enum_to_type = typename implementation::enum_to_type_<T>::type;
+
+	static constexpr bool is_primative_type(const GLenum aType) {
+		return
+			aType == GL_UNSIGNED_BYTE ||
+			aType == GL_BYTE ||
+			aType == GL_UNSIGNED_SHORT ||
+			aType == GL_SHORT ||
+			aType == GL_UNSIGNED_INT ||
+			aType == GL_INT ||
+			aType == GL_FLOAT ||
+			aType == GL_DOUBLE;
+	}
+}}
+
 #endif
